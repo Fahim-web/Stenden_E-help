1ac8b0817abc5c31cf3a684c356d4cecb76b5fbe
Aleksei Skorjak <43403562+AlekseiSkr@users.noreply.github.com>
2020-01-13 15:43:44 +0100

    Added basis for header.php
    some

---
+++ include/header.php
@@ -0,0 +1,22 @@
+<?php
+    include ('./html/head.html');
+    include ('connect.php');
+    include ('session.php');
+?>
+
+<body>
+
+
+  
+    <header>
+        <div class="container"> 
+        <a href="index.html" ><img class="logo" src="img/logo.png"></a>
+        <div class="menu-btn not-active"><span></span>
+            </div>
+        <ul class="menu"><li><a href="#">faq</a></li>
+                <li><a href="#">contact us</a></li>
+                <li><a href="login">login/register</a></li>
+                <li><a href="#"></a></li>
+        </ul>
+        </div>
+    </header>
