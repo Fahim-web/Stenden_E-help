b046d3e3e226f74f65d3cbc14864f863d99d0996
Aleksei Skorjak <43403562+AlekseiSkr@users.noreply.github.com>
2020-01-13 16:09:46 +0100

    Update header.php

--- include/header.php
+++ include/header.php
@@ -6,17 +6,41 @@
 
 <body>
 
+<?php
 
-  
-    <header>
-        <div class="container"> 
-        <a href="index.html" ><img class="logo" src="img/logo.png"></a>
-        <div class="menu-btn not-active"><span></span>
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+
+    echo $_SESSION['operatorID'];
+    echo $_SESSION['customerID'];
+
+
+echo "
+
+<header>
+        <div class='container'> 
+        <a href='index.html' ><img class='logo' src='.../img/logo.png'></a>
+        <div class='menu-btn not-active'><span></span>
             </div>
-        <ul class="menu"><li><a href="#">faq</a></li>
-                <li><a href="#">contact us</a></li>
-                <li><a href="login">login/register</a></li>
-                <li><a href="#"></a></li>
+
+        <ul class='menu'><li><a href='#'>faq</a></li>
+        ";
+        if (isset($_SESSION['operatorID'])){
+            $sql = 'SELECT Clearance FROM operator WHERE OperatorID = ?;';
+        }
+        
+        
+        echo "
+                <li><a href='#'>contact us</a></li>
+                <li><a href='login'>login/register</a></li>
+                <li><a href='#'></a></li>
         </ul>
         </div>
     </header>
+
+
+";
+}
+
+?>
+  
+    
