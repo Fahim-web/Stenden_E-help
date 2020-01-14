dde55f2ad77f5852acf8f9e3e3cb46379d3c39e0
jabalii <59643276+jabalii@users.noreply.github.com>
2020-01-14 13:58:53 +0100

    login repair

--- php_process/header_log_button.php
+++ php_process/header_log_button.php
@@ -1,5 +1,5 @@
 <?php
-                            if (($_SESSION['loggedIn'])){
+                            if (isset($_SESSION['loggedIn'])){
                                 echo "
                                         <form action='./php_process/log_out.php' method='post'>
                                         <li><input id='log' type='submit' value='Log out' name='logout'></li>
@@ -10,4 +10,3 @@
                                          <li><a href='./php_process/login.php'>login/register</a></li>
                                     ";
                             }
-                        ?>
