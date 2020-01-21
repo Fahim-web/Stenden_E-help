e4e8662c753ca6792312e723fee1ecd95ece2afa
jabalii <59643276+jabalii@users.noreply.github.com>
2020-01-21 10:26:18 +0100

    Update ticket_maintanance.php

--- php_process/ticket_maintanance.php
+++ php_process/ticket_maintanance.php
@@ -8,7 +8,7 @@ include('header.php');
 if (isset($_GET['maintain'])) {
     $exp = explode(":", $_GET['maintain']);
 
-        $sql = "SELECT i.incidentid,i.RegisteredBy,i.description,i.report_date, i.resolution_date, i.topic,o.operator_name,li.description,t.description,s.StatusID,o.operatorid, cmp.name, o.filepath, c.filepath, c.username, c.email, sol.SolutionID, sol.Description, f.description
+    $sql = "SELECT i.incidentid,i.RegisteredBy,i.description,i.report_date, i.resolution_date, i.topic,o.operator_name,li.description,t.description,s.StatusID,o.operatorid, cmp.name, o.filepath, c.filepath, c.username, c.email, sol.SolutionID, sol.Description, f.description
         FROM incident as i, operator as o, customer as c, type as t, status as s,company as cmp, license as li , solution as sol, frequency as f
         WHERE i.operatorid=o.operatorid AND i.typeID=t.typeID AND i.StatusID=s.StatusID AND i.customerID=c.customerID AND cmp.companyID=c.companyID AND cmp.LicenseID=li.LicenseID AND i.SolutionID=sol.SolutionID AND i.FrequencyID=f.FrequencyID
         AND i.customerID= " . $exp[1] . " AND i.incidentid = " . $exp[0] . ";";
@@ -20,13 +20,33 @@ if (isset($_GET['maintain'])) {
             exit();
         }
         //header('Location:ticket_maintanance.php?maintain=');
-        mysqli_stmt_bind_result($stmt, $incidentID, $registered_by, $description, $rep_date, $res_date, $topic, $operatorName,
-        $license, $typeDesc, $status, $opeID, $companyName, $opPic, $custPic, $custUsername, $custEmail, $solID, $solution, $frequency);
+        mysqli_stmt_bind_result(
+            $stmt,
+            $incidentID,
+            $registered_by,
+            $description,
+            $rep_date,
+            $res_date,
+            $topic,
+            $operatorName,
+            $license,
+            $typeDesc,
+            $status,
+            $opeID,
+            $companyName,
+            $opPic,
+            $custPic,
+            $custUsername,
+            $custEmail,
+            $solID,
+            $solution,
+            $frequency
+        );
         mysqli_stmt_store_result($stmt);
 
         while (mysqli_stmt_fetch($stmt)) {
-     echo
-     '
+            echo
+                '
      <body>
      <div class="content_wrapper">
     <div class="maintain_wrapper">
@@ -35,7 +55,7 @@ if (isset($_GET['maintain'])) {
                 <img id="profilePic" src="' . $custPic . '" alt="Profile picture">
                 <div class="maintain_client_info_text">
                     <h4>Name: ' . $custUsername . '</h4>
-                    <h4>Comapny: '. $companyName .'</h4>
+                    <h4>Comapny: ' . $companyName . '</h4>
                     <h4>Email: ' . $custEmail . '</h4>
                 </div>
             </div>
@@ -51,36 +71,32 @@ if (isset($_GET['maintain'])) {
     <div class="maintain_wrapper">
         <div class="maintain_client">
             <div class="maintain_client_info">
-                <p>Date Reported: '.$rep_date.'</p>
-                <p>Deadline Date: '.$res_date.'</p>
-                <p>Registered By: '.$registered_by.'</p>
-                <p>Type: '.$typeDesc.'</p>
-                <p>Frequency: '.$frequency.'</p>
+                <p>Date Reported: ' . $rep_date . '</p>
+                <p>Deadline Date: ' . $res_date . '</p>
+                <p>Registered By: ' . $registered_by . '</p>
+                <p>Type: ' . $typeDesc . '</p>
+                <p>Frequency: ' . $frequency . '</p>
             </div>';
 
-            if ($solID !== 5){
+            if ($solID !== 5) {
                 echo '
             <div class="maintain_client_text">
                 <!--IF TEXT APPEARS PUT TEXT INTO A .bubble_response(different color bubble with different dirrextion of a tail)-->
                 <div class="bubble_response">';
 
-                    echo '<p>'.$solution.'</p>
+                echo '<p>' . $solution . '</p>
                     </div>
                 </div>
             </div>
         </div>
         </div>
             ';
-                }
-                echo '
+            }
+            echo '
                 </div>
         </div>
         <form id="maintain_form" action="" method="POST">
         ';
-
-
-
-
         }
     }
 } else {
@@ -90,11 +106,11 @@ if (isset($_GET['maintain'])) {
 
 
 
-    //<!--START OF THE OPERATOR INPUT-->
+//<!--START OF THE OPERATOR INPUT-->
 
 
-    if (isset($clearance) && $solID == '5'  && $opeID == $_SESSION['operatorId']){
-            echo "
+if (isset($clearance) && $solID == '5'  && $opeID == $_SESSION['operatorId']) {
+    echo "
             <div class='maintain_client_anwserBar'>
                 <div class='maintain_client_anwserBar_wrap clearfix'>
                     <div class='maintain_client_anwserBar_text'>
@@ -111,68 +127,66 @@ if (isset($_GET['maintain'])) {
             </div>
             ";
 
-            
-            $response = htmlentities($_POST['response']);
-            if(!empty($response) && isset($_POST['status'])){
-                if (isset($_POST['status'])){
-                     $status = 3;
-                 }
-                 $sql_insert_solution = 'INSERT INTO solution VALUES(NULL, ?)';
 
-                if ($stmt_insert = mysqli_prepare($con, $sql_insert_solution)){
-                     mysqli_stmt_bind_param(
-                         $stmt_insert,
-                         's',
-                         $response
-                     );
-                    $exec_insert = mysqli_stmt_execute($stmt_insert);
-                    if ($exec_insert == FALSE) {
-                        header('Location:ticket_client.php?error=SelectIssue');
-                        
-                    }
-                    mysqli_stmt_close($stmt_insert);
+    // $response = htmlentities($_POST['response']);
+    if (!empty($_POST['response']) && isset($_POST['status'])) {
+        $response = htmlentities($_POST['response']);
+        if (isset($_POST['status'])) {
+            $status = 3;
+        }
+        $sql_insert_solution = 'INSERT INTO solution VALUES(NULL, ?)';
 
-                }
+        if ($stmt_insert = mysqli_prepare($con, $sql_insert_solution)) {
+            mysqli_stmt_bind_param(
+                $stmt_insert,
+                's',
+                $response
+            );
+            $exec_insert = mysqli_stmt_execute($stmt_insert);
+            if ($exec_insert == FALSE) {
+                header('Location:ticket_client.php?error=SelectIssue');
+            }
+            mysqli_stmt_close($stmt_insert);
+        }
 
-                    $sql_select_solutionID = 'SELECT SolutionID FROM solution WHERE Description = ?';
-                    if($stmt_select = mysqli_prepare($con, $sql_select_solutionID)){
-                        mysqli_stmt_bind_param($stmt_select, 's', $response);
-                        $exec_select = mysqli_stmt_execute($stmt_select);
-                        mysqli_stmt_bind_result($stmt_select, $response);
-                        mysqli_stmt_store_result($stmt_select);
-                        if (mysqli_stmt_num_rows($stmt_select) == 0) {
-                            header('Location:ticket_client.php?error=NoRowsFound');
+        $sql_select_solutionID = 'SELECT SolutionID FROM solution WHERE Description = ?';
+        if ($stmt_select = mysqli_prepare($con, $sql_select_solutionID)) {
+            mysqli_stmt_bind_param($stmt_select, 's', $response);
+            $exec_select = mysqli_stmt_execute($stmt_select);
+            mysqli_stmt_bind_result($stmt_select, $response);
+            mysqli_stmt_store_result($stmt_select);
+            if (mysqli_stmt_num_rows($stmt_select) == 0) {
+                header('Location:ticket_client.php?error=NoRowsFound');
+                exit();
+            } else {
+                $sql_update_incident = 'UPDATE incident SET StatusID = ?, SolutionID = ? WHERE IncidentID =' . $incidentID . ';';
+                while (mysqli_stmt_fetch($stmt_select)) {
+                    if ($stmt_update = mysqli_prepare($con, $sql_update_incident)) {
+                        mysqli_stmt_bind_param($stmt_update, 'is', $status, $response);
+                        $exec_update = mysqli_stmt_execute($stmt_update);
+                        header('Location:ticket_maintanance.php?maintain=' . $incidentID . ':' . $exp[1] . '');
+
+                        if ($exec_update == FALSE) {
+                            echo "pizda";
                             exit();
-                        } else {
-                            $sql_update_incident = 'UPDATE incident SET StatusID = ?, SolutionID = ? WHERE IncidentID ='.$incidentID.';';
-                            while(mysqli_stmt_fetch($stmt_select)){
-                                if ($stmt_update = mysqli_prepare($con, $sql_update_incident)){
-                                    mysqli_stmt_bind_param($stmt_update, 'is', $status, $response);
-                                    $exec_update = mysqli_stmt_execute($stmt_update);
-                                    header('Location:ticket_maintanance.php?maintain=' . $incidentID . ':' . $exp[1] . '');
-                                    
-                                    if ($exec_update == FALSE) {
-                                        echo "pizda";
-                                        exit();
-                                    }
-                                    mysqli_stmt_close($stmt_update);
-                                    exit();
-                                    
-                                }
-                            }
                         }
-
-                        mysqli_stmt_close($stmt_select);
-                    }else{
-                        echo "fill in the fields";
-                    }         
+                        mysqli_stmt_close($stmt_update);
+                        exit();
+                    }
                 }
             }
-            unset($response);
+
+            mysqli_stmt_close($stmt_select);
+        } else {
+            echo "fill in the fields";
+        }
+    }
+}
+unset($response);
 
 
-            //<!--END OF OPERATOR INPUT-->
-    echo "
+//<!--END OF OPERATOR INPUT-->
+echo "
     </form>
 
     </div>";
